b5278e1f1ee59ebd79c9f18f3c009d65cd410b90
luhan-gui <luhan.guiggia@medjed-dev.fr>
2026-02-18 00:05:52 +0100

    Ajout mixin et modif scss

--- index.php
+++ index.php
@@ -8,15 +8,15 @@
 </head>
 <body>
     <nav>
-        <h1>Luhan Guiggia-Milon</h1>
-            <a href="https://www.linkedin.com/in/luhan-guiggia/">
+        <h1>Luhan Guiggia--Milon</h1>
+            <a class="linkedin" href="https://www.linkedin.com/in/luhan-guiggia/">
                 <svg width="44" height="42" viewBox="0 0 44 42" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M30 14C33.1826 14 36.2348 15.2643 38.4853 17.5147C40.7357 19.7652 42 22.8174 42 26V40H34V26C34 24.9391 33.5786 23.9217 32.8284 23.1716C32.0783 22.4214 31.0609 22 30 22C28.9391 22 27.9217 22.4214 27.1716 23.1716C26.4214 23.9217 26 24.9391 26 26V40H18V26C18 22.8174 19.2643 19.7652 21.5147 17.5147C23.7652 15.2643 26.8174 14 30 14Z" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
                     <path d="M10 16H2V40H10V16Z" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
                     <path d="M6 10C8.20914 10 10 8.20914 10 6C10 3.79086 8.20914 2 6 2C3.79086 2 2 3.79086 2 6C2 8.20914 3.79086 10 6 10Z" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
             </a>
-            <a href="https://github.com/luhan-gui">
+            <a class="github" href="https://github.com/luhan-gui">
                 <svg width="43" height="46" viewBox="0 0 43 46" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M16.0005 38C6.00049 41 6.00049 33 2.00049 32M30.0005 44V36.26C30.0755 35.3063 29.9467 34.3476 29.6225 33.4476C29.2984 32.5476 28.7863 31.7268 28.1205 31.04C34.4005 30.34 41.0005 27.96 41.0005 17.04C41 14.2477 39.9259 11.5624 38.0005 9.54C38.9122 7.09701 38.8477 4.3967 37.8205 2C37.8205 2 35.4605 1.3 30.0005 4.96C25.4165 3.71764 20.5845 3.71764 16.0005 4.96C10.5405 1.3 8.18049 2 8.18049 2C7.15324 4.3967 7.08878 7.09701 8.00049 9.54C6.06074 11.5774 4.98553 14.2869 5.00049 17.1C5.00049 27.94 11.6005 30.32 17.8805 31.1C17.2225 31.7799 16.715 32.5908 16.3911 33.4798C16.0672 34.3689 15.9341 35.3161 16.0005 36.26V44" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
@@ -26,57 +26,62 @@
         <h2>Bonjour!</h2>
         <p>Je suis Guiggia--Milon Luhan,<br> Étudiant en BUT Informatique <br> à l'IUT de Lannion</p>
         <img src="img/header.png" alt="Luhan GUIGGIA--MILON dans un écran indiquant de scroller la page vers le bas">
+        <section>
+            <button class="btn_parcours">Mon parcours</button>
+            <a class="btn_cv" href="/assets/CV_Luhan_Guiggia-Milon.pdf" target="_blank" rel="noopener noreferrer">
+                Mon CV
+            </a>
+            <button class="btn_a_propos">
+                À propos de moi
+            </button>
+        </section>
     </header>
-    <button class="btn_parcours">Mon parcours</button>
-    <a class="btn_parcours" href="/assets/CV_Luhan_Guiggia-Milon.pdf" target="_blank" rel="noopener noreferrer">
-        Mon CV
-    </a>
-    <button>
-        À propos de moi
-    </button>
-    <h2>Mes compétences</h2>
-    <h3>Hard Skills</h3>
-    <section class="hardskills">
-        <div>
-            <h4>Programmation</h4>
-            <img src="" alt="Langage C">
-            <img src="" alt="Langage Java">
-            <img src="" alt="Langage Python">
-        </div>
-        <div>
-            <h4>Développement Web</h4>
-            <img src="" alt="HTML5">
-            <img src="" alt="CSS 3">
-            <img src="" alt="PHP">
-            <img src="" alt="JS">
-        </div>
-        <div>
-            <h4>Base De Données</h4>
-            <img src="" alt="PostgreSQL">
-            <img src="" alt="Rel">
-        </div>
-        <div>
-            <h4>Automatisation</h4>
-            <img src="" alt="BASH">
-            <img src="" alt="Docker">
-        </div>
-        <div>
-            <h4>Travail Collaboratif</h4>
-            <img src="" alt="GIT">
-            <img src="" alt="Teams">
-            <img src="" alt="Jira">
-        </div>
+    <section class="competence">
+        <h2>Mes compétences</h2>
+        <h3>Hard Skills</h3>
+        <section class="hardskills">
+            <div>
+                <h4>Programmation</h4>
+                <img src="" alt="Langage C">
+                <img src="" alt="Langage Java">
+                <img src="" alt="Langage Python">
+            </div>
+            <div>
+                <h4>Développement Web</h4>
+                <img src="" alt="HTML5">
+                <img src="" alt="CSS 3">
+                <img src="" alt="PHP">
+                <img src="" alt="JS">
+            </div>
+            <div>
+                <h4>Base De Données</h4>
+                <img src="" alt="PostgreSQL">
+                <img src="" alt="Rel">
+            </div>
+            <div>
+                <h4>Automatisation</h4>
+                <img src="" alt="BASH">
+                <img src="" alt="Docker">
+            </div>
+            <div>
+                <h4>Travail Collaboratif</h4>
+                <img src="" alt="GIT">
+                <img src="" alt="Teams">
+                <img src="" alt="Jira">
+            </div>
+        </section>
+        <h3>Compétences Académiques</h3>
+        <button>Réaliser un développement d'application</button>
+        <button>Optimiser des applications informatiques</button>
+        <button>Administrer des systèmes informatiques communicants complexes</button>
+        <button>Gérer les données de l'information</button>
+        <button>Conduire un projet</button>
+        <button>Travailler dans une équipe informatique</button>
     </section>
-    <h3>Compétences Académiques</h3>
-    <button>Réaliser un développement d'application</button>
-    <button>Optimiser des applications informatiques</button>
-    <button>Administrer des systèmes informatiques communicants complexes</button>
-    <button>Gérer les données de l'information</button>
-    <button>Conduire un projet</button>
-    <button>Travailler dans une équipe informatique</button>
     <h2>Mon Portfolio</h2>
     <h3>Mes Projets Personnels</h3>
     <h3>Mes Projets Académiques</h3>
     <h3>Mes Projets Professionnels</h3>
+
 </body>
 </html>
